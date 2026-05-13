7f9fd8a63d5a102eee3463331e5545f1264708bb
Maaartinnn <282541141+Maaartinnn@users.noreply.github.com>
2026-05-14 00:00:47 +0800

    build: 优化dev构建脚本

--- src/dev-build.cmd
+++ src/dev-build.cmd
@@ -1,1 +1,2 @@
-cd ..\web && npm run build && cd ..\src && go build -ldflags="-s -w" -o v2rayN-Go.exe .
+@echo off
+cd ..\web && npm run build && cd ..\src && go build -ldflags="-s -w" -o v2rayN-Go.exe . && echo successful build && pause
